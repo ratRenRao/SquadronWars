56da7ae6b5b9412122686cbcccdf30e1bf02b7bc
Clint Fowler <jedd1876@gmail.com>
2015-11-17 00:40:40 -0700

    Initial folder structure and common classes to be built

---
+++ API/src/Config/init.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Clint Fowler
+ * Date: 11/16/2015
+ * Time: 11:52 PM
+ *
+ * will be used to initialize slim framework on server.
+ */
---
+++ API/src/app.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Clint Fowler
+ * Date: 11/16/2015
+ * Time: 11:55 PM
+ *
+ * will use this class to create our website endpoints.
+ */
+
