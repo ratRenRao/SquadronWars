9da0f855444216e09a04b761b59c956f5c7eb02b
Clint Fowler <jedd1876@gmail.com>
2015-11-17 20:16:49 -0700

    Pushing latest changes

---
+++ API/html/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Clint Fowler
+ * Date: 11/17/2015
+ * Time: 3:53 PM
+ */
+
+//Initialize Slim Framework to manage website
+require_once realpath(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'Config'.DIRECTORY_SEPARATOR.'init.php');
+
+
