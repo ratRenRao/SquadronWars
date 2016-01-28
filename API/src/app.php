cb58bedf294e4a24ec5928e640ec4815918c83ce
Clint Fowler <jedd1876@gmail.com>
2016-01-27 20:42:26 -0700

    Implement API endpoints for UpdatePlayer and UpdateCharacter

--- API/src/app.php
+++ API/src/app.php
@@ -19,8 +19,9 @@ $app = new \Silex\Application();
 
 
 
-$app->get('/',function() use($app){
-
+$app->get('/',function() use($app)
+{
+    //loads landing page. under construction
     require_once commonpath.DIRECTORY_SEPARATOR.'html'.DIRECTORY_SEPARATOR.'index.html';
     return '';
 
@@ -29,21 +30,22 @@ $app->get('/',function() use($app){
 /**
  * API end points to implement all of our database calls.
  */
-$app->get('/api', function() use($app) {
-
+$app->get('/api', function() use($app)
+{
     return 'Welcome to the API ';
 });
 
-$app->post('/api/auth', function() use($app) {
-
-
-    //Set our database scheme to be used here.
+$app->post('/api/auth', function() use($app)
+{
+    //create database object to be used.
     $mysql = new MySQL();
 
+    //verify post request contains username and password.
     if(isset($_POST['username']) && isset($_POST['password']))
     {
         $returnobject["PlayerInfo"] = $mysql->authenticateUser($app->escape($_POST['username']),$app->escape($_POST['password']));
 //       $returnobject["PlayerInfo"] = $mysql->authenticateUser("test","testing123");
+
 
         if(sizeof($returnobject) > 0)
         {
@@ -54,48 +56,95 @@ $app->post('/api/auth', function() use($app) {
         }
     }
 
-    return new Response("Failed Authentication",401);
+    return new Response("Failed Authentication", 401);
 });
 
-$app->post('/api/UpdateCharacter', function() use($app) {
+$app->post('/api/UpdateCharacter', function() use($app)
+{
+    //create database object to be used.
     $mysql = new MySQL();
+    $response = 401;
 
+    //verify post request contains username and password.
     if(isset($_POST['username']) && isset($_POST['password']))
     {
-
+        $verifyPlayer = $mysql->authenticateUser($app->escape($_POST['username']),$app->escape($_POST['password']));
+        if(sizeof($verifyPlayer) > 0)
+        {
+            $response = $mysql->updateCharacter($_POST['player'], json_decode($_POST['character']));
+        }
     }
-    //TODO: update character
-    return new Response("Failed",401);
-});
 
-$app->post('/api/UpdatePlayer', function() use($app) {
-    $mysql = new MySQL();
-
-    if(isset($_POST['username']) && isset($_POST['password']))
+    if($response == 200)
     {
-
+        return new Response("Successful", 200);
     }
-    //TODO: update player
-    return new Response("Failed",401);
+    else if($response == 500)
+    {
+        return new Response("Server Error", 500);
+    }
+    else
+    {
+        return new Response("Failed",401);
+    }
 });
 
-$app->post('/api/UpdateSquad', function() use($app) {
+$app->post('/api/UpdatePlayer', function() use($app)
+{
+    //create database object to be used.
     $mysql = new MySQL();
+    $response = 401;
+
+    //verify post request contains username and password.
 
     if(isset($_POST['username']) && isset($_POST['password']))
     {
+        $verifyPlayer = $mysql->authenticateUser($app->escape($_POST['username']),$app->escape($_POST['password']));
+        if(sizeof($verifyPlayer) > 0)
+        {
+            $response = $mysql->updatePlayer(json_decode($_POST['player']));
+        }
+    }
+    if($response == 200)
+    {
+        return new Response("Successful", 200);
+    }
+    else if($response == 500)
+    {
+        return new Response("Server Error", 500);
+    }
+    else
+    {
+        return new Response("Failed",401);
+    }
+});
 
+$app->post('/api/UpdateSquad', function() use($app)
+{
+    //create database object to be used.
+    $mysql = new MySQL();
+
+    //verify post request contains username and password.
+    if(isset($_POST['username']) && isset($_POST['password']))
+    {
+        $verifyPlayer = $mysql->authenticateUser($app->escape($_POST['username']),$app->escape($_POST['password']));
+        if(sizeof($verifyPlayer) > 0)
+        {
+
+        }
     }
     //TODO: update squad
     return new Response("Failed",401);
 });
 
-$app->post('/api/StartGame', function() use($app) {
+$app->post('/api/StartGame', function() use($app)
+{
     //TODO: Start Game
     return new Response("Failed",401);
 });
 
-$app->post('/api/GetGameInfo', function() use($app) {
+$app->post('/api/GetGameInfo', function() use($app)
+{
     //TODO: Check Game status
     return new Response("Failed",401);
 });
