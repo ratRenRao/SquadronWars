c166c36cc052a25dcc62e3482b081c960ffd4fb3
Clint Fowler <jedd1876@gmail.com>
2016-02-14 21:40:23 -0700

    Adjusted database calls to match our new layout.

--- API/src/Common/DBStructure/MySQL.php
+++ API/src/Common/DBStructure/MySQL.php
@@ -17,12 +17,17 @@ use PDO;
 //we will need to implement the interface on this class. waiting until we have our interface defined.
 class MySQL implements IDBStructure
 {
-    //will return status code for response message.
-    public function updatePlayer($playerObject)
+
+    /*
+     * function to authenticate user to database. will need to be changed to match our system calls and return
+     * needed information back from the database. some modification will need to be done.
+     *
+     */
+    public function authenticateUser($username, $password)
     {
+
         //create database reference object
-        $dbh = '';
-        $returnCode = 500;
+        $dbh='';
 
         //Try to connect to mysql service
         try
@@ -34,32 +39,210 @@ class MySQL implements IDBStructure
         {
             //this will be ignored in production. do not want to echo back this error.
             //echo $e->getMessage();
-
-            //return status code to be used in response message. 500 server error.
-            return $returnCode;
         }
 
-        //prepare SQL statemenet
-        $query = $dbh->prepare("CALL sp_UpdatePlayer(?,?,?,?,?)");
-        $query->bindParam(1, $playerObject->{"username"},  PDO::PARAM_STR);
-        $query->bindParam(2, $playerObject->{"password"},  PDO::PARAM_STR);
-        $query->bindParam(3, $playerObject->{"firstname"}, PDO::PARAM_STR);
-        $query->bindParam(4, $playerObject->{"lastname"},  PDO::PARAM_STR);
-        $query->bindParam(5, $playerObject->{"email"},     PDO::PARAM_STR);
+        //Prepare SQL statement and bind the parameters for authenticating a username and password.
+        $query = $dbh->prepare("CALL sp_AuthorizePlayer(?,?)");
+        $query->bindParam(1, $username, PDO::PARAM_STR);
+        $query->bindParam(2, $password, PDO::PARAM_STR);
 
+        //Execute stored procedure with username and password passed in.
+        $query->execute();
 
-        //execute procedure.
-       $query->execute();
+        //Turns the result from the stored procedure (if any) into an associative array.
+        $results = $query->fetch(PDO::FETCH_ASSOC);
 
-        if($query->rowCount() > 0)
+        //done with database connection. Closing connection.
+        $query->closeCursor();
+
+        if(!$results)
         {
-            //return status code to be used in response message. 200 successful.
-            $returnCode = 200;
+            return;
+        }
+
+        //if there are rows in result set from the query, then successful authentication.
+        if(sizeof($results) > 0)
+        {
+            //generate a session id to track logged in time store into an array to return
+            $results["SessionID"] = "test";
+
+            //return Player information.
+            return $results;
+        }
+
+        //failed authentication, we return nothing.
+        return;
+    }
+
+    public function createCharacter($characterObject)
+    {
+        // TODO: Implement createCharacter() method.
+        //create database reference object
+        $dbh='';
+
+        //Try to connect to mysql service
+        try
+        {
+            //created config file to hold user name and password that we will use to obscure and keep off of our repo.
+            $dbh = new PDO("mysql:host=localhost:3306;dbname=dbo", dbuser, dbpass);
+        }
+        catch(PDOException $e)
+        {
+            //this will be ignored in production. do not want to echo back this error.
+            //echo $e->getMessage();
+            return;
+        }
+
+        $query = $dbh->prepare("CALL sp_AuthorizePlayer(?,?)");
+        $query->bindParam(1, $characterObject->{"username"}, PDO::PARAM_STR);
+        $query->bindParam(2, $characterObject->{"password"}, PDO::PARAM_STR);
+
+        $query->execute();
+
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+
+        $query->closeCursor();
+
+        if(sizeof($result) < 1)
+        {
+            return;
+        }
+
+        $query = $dbh->prepare("CALL sp_CreatePlayer(?,?)");
+        $query->bindParam(1, $characterObject->{"PlayerID"}, PDO::PARAM_INT);
+        $query->bindParam(2, $characterObject->{"CharacterName"}, PDO::PARAM_STR);
+
+
+    }
+
+    public function createPlayer($register)
+    {
+        // TODO: Implement createPlayer() method.
+        //create database reference object
+        $dbh='';
+
+        //Try to connect to mysql service
+        try
+        {
+            //created config file to hold user name and password that we will use to obscure and keep off of our repo.
+            $dbh = new PDO("mysql:host=localhost:3306;dbname=dbo", dbuser, dbpass);
+        }
+        catch(PDOException $e)
+        {
+            //this will be ignored in production. do not want to echo back this error.
+            echo $e->getMessage();
+        }
+
+        $query = $dbh->prepare("CALL sp_CreatePlayer(?,?,?,?,?)");
+        $query->bindParam(1,$register->{"username"},PDO::PARAM_STR);
+        $query->bindParam(2,$register->{"password"},PDO::PARAM_STR);
+        $query->bindParam(3,$register->{"firstname"},PDO::PARAM_STR);
+        $query->bindParam(4,$register->{"lastname"},PDO::PARAM_STR);
+        $query->bindParam(5,$register->{"email"},PDO::PARAM_STR);
+
+        $query->execute();
+
+        $return = null;
+
+        if($query->rowCount() == 1)
+        {
+            $return["PlayerInfo"] = $this->authenticateUser($register->{"username"}, $register->{"password"});
+            $return["PlayerDetails"] = $this->getPlayer($return["PlayerInfo"]["playerId"]);
         }
 
         $query->closeCursor();
 
-        return $returnCode;
+        return $return;
+
+
+    }
+
+    public function getCharacters($playerID)
+    {
+
+        //create database reference object
+        $dbh='';
+
+        //Try to connect to mysql service
+        try
+        {
+            //created config file to hold user name and password that we will use to obscure and keep off of our repo.
+            $dbh = new PDO("mysql:host=localhost:3306;dbname=dbo", dbuser, dbpass);
+        }
+        catch(PDOException $e)
+        {
+            //this will be ignored in production. do not want to echo back this error.
+            echo $e->getMessage();
+        }
+
+
+        $query = $dbh->prepare("CALL sp_GetCharacters(?)");
+        $query->bindParam(1, $playerID, PDO::PARAM_INT);
+        $query->execute();
+        $returnObject = $query->fetchAll(PDO::FETCH_ASSOC);
+        $query->closeCursor();
+
+        for($i = 0; $i < sizeof($returnObject); $i++)
+        {
+            $query = $dbh->prepare("CALL sp_GetCharacterAbilities(?)");
+            $query->bindParam(1, $returnObject[$i]["characterId"], PDO::PARAM_INT);
+            $query->execute();
+            $returnObject[$i]["CharacterAbilities"] = $query->fetchAll(PDO::FETCH_ASSOC);
+            $query->closeCursor();
+        }
+
+
+        //done with database connection. Closing connection.
+        $query->closeCursor();
+
+        return $returnObject;
+
+    }
+
+    public function getPlayer($playerID)
+    {
+        //create database reference object
+        $dbh='';
+
+        //Try to connect to mysql service
+        try
+        {
+            //created config file to hold user name and password that we will use to obscure and keep off of our repo.
+            $dbh = new PDO("mysql:host=localhost:3306;dbname=dbo", dbuser, dbpass);
+        }
+        catch(PDOException $e)
+        {
+            //this will be ignored in production. do not want to echo back this error.
+            echo $e->getMessage();
+        }
+
+        $returnObject["Characters"] = $this->getCharacters($playerID);
+
+        $query = $dbh->prepare("CALL sp_GetSquads(?)");
+        $query->bindParam(1, $playerID, PDO::PARAM_INT);
+        $query->execute();
+        $returnObject["Squads"] = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $query = $dbh->prepare("CALL sp_GetInventory(?)");
+        $query->bindParam(1, $playerID, PDO::PARAM_INT);
+        $query->execute();
+        $returnObject["Inventory"] = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $query = $dbh->prepare("CALL sp_GetAbilities");
+        $query->execute();
+        $returnObject["Abilities"] = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $query = $dbh->prepare("CALL sp_GetAbilitiesPreReqs");
+        $query->execute();
+        $returnObject["AbilityPreReqs"] = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $query = $dbh->prepare("CALL sp_GetItems");
+        $query->execute();
+        $returnObject["Items"] = $query->fetchAll(PDO::FETCH_ASSOC);
+
+        $query->closeCursor();
+
+        return $returnObject;
     }
 
     public function updateCharacter($playerID, $characterObject)
@@ -132,16 +315,12 @@ class MySQL implements IDBStructure
         return $returnCode;
     }
 
-    /*
-     * function to authenticate user to database. will need to be changed to match our system calls and return
-     * needed information back from the database. some modification will need to be done.
-     *
-     */
-    public function authenticateUser($username, $password)
+    //will return status code for response message.
+    public function updatePlayer($playerObject)
     {
-
         //create database reference object
-        $dbh='';
+        $dbh = '';
+        $returnCode = 500;
 
         //Try to connect to mysql service
         try
@@ -153,172 +332,31 @@ class MySQL implements IDBStructure
         {
             //this will be ignored in production. do not want to echo back this error.
             //echo $e->getMessage();
+
+            //return status code to be used in response message. 500 server error.
+            return $returnCode;
         }
 
-        //Prepare SQL statement and bind the parameters for authenticating a username and password.
-        $query = $dbh->prepare("CALL sp_AuthorizePlayer(?,?)");
-        $query->bindParam(1, $username, PDO::PARAM_STR);
-        $query->bindParam(2, $password, PDO::PARAM_STR);
+        //prepare SQL statemenet
+        $query = $dbh->prepare("CALL sp_UpdatePlayer(?,?,?,?,?)");
+        $query->bindParam(1, $playerObject->{"username"},  PDO::PARAM_STR);
+        $query->bindParam(2, $playerObject->{"password"},  PDO::PARAM_STR);
+        $query->bindParam(3, $playerObject->{"firstname"}, PDO::PARAM_STR);
+        $query->bindParam(4, $playerObject->{"lastname"},  PDO::PARAM_STR);
+        $query->bindParam(5, $playerObject->{"email"},     PDO::PARAM_STR);
 
-        //Execute stored procedure with username and password passed in.
+
+        //execute procedure.
         $query->execute();
 
-        //Turns the result from the stored procedure (if any) into an associative array.
-        $results = $query->fetch(PDO::FETCH_ASSOC);
-
-        //done with database connection. Closing connection.
-        $query->closeCursor();
-
-        if(!$results)
+        if($query->rowCount() > 0)
         {
-            return;
-        }
-
-        //if there are rows in result set from the query, then successful authentication.
-        if(sizeof($results) > 0)
-        {
-            //generate a session id to track logged in time store into an array to return
-            $results["SessionID"] = "test";
-
-            //return Player information.
-            return $results;
-        }
-
-        //failed authentication, we return nothing.
-        return;
-    }
-
-    public function getPlayer($playerID)
-    {
-        //create database reference object
-        $dbh='';
-
-        //Try to connect to mysql service
-        try
-        {
-            //created config file to hold user name and password that we will use to obscure and keep off of our repo.
-            $dbh = new PDO("mysql:host=localhost:3306;dbname=dbo", dbuser, dbpass);
-        }
-        catch(PDOException $e)
-        {
-            //this will be ignored in production. do not want to echo back this error.
-            echo $e->getMessage();
-        }
-
-        $query = $dbh->prepare("CALL sp_GetCharacters(?)");
-        $query->bindParam(1, $playerID, PDO::PARAM_INT);
-        $query->execute();
-        $returnObject["Characters"] = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        $query = $dbh->prepare("CALL sp_GetCharacterAbilities(?)");
-        $query->bindParam(1, $playerID, PDO::PARAM_INT);
-        $query->execute();
-        $returnObject["CharacterAbilities"] = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        $query = $dbh->prepare("CALL sp_GetInventory(?)");
-        $query->bindParam(1, $playerID, PDO::PARAM_INT);
-        $query->execute();
-        $returnObject["Inventory"] = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        $query = $dbh->prepare("CALL sp_GetAbilities");
-        $query->execute();
-        $returnObject["Abilities"] = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        $query = $dbh->prepare("CALL sp_GetAbilitiesPreReqs");
-        $query->execute();
-        $returnObject["AbilityPreReqs"] = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        $query = $dbh->prepare("CALL sp_GetEquipment");
-        $query->execute();
-        $returnObject["Equipment"] = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        $query = $dbh->prepare("CALL sp_GetSquads(?)");
-        $query->bindParam(1, $playerID, PDO::PARAM_INT);
-        $query->execute();
-        $returnObject["Squads"] = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        $query = $dbh->prepare("CALL sp_GetItems");
-        $query->execute();
-        $returnObject["Items"] = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        $query->closeCursor();
-
-        return $returnObject;
-    }
-
-    public function getCharacters($playerID)
-    {
-
-        //create database reference object
-        $dbh='';
-
-        //Try to connect to mysql service
-        try
-        {
-            //created config file to hold user name and password that we will use to obscure and keep off of our repo.
-            $dbh = new PDO("mysql:host=localhost:3306;dbname=dbo", dbuser, dbpass);
-        }
-        catch(PDOException $e)
-        {
-            //this will be ignored in production. do not want to echo back this error.
-            echo $e->getMessage();
-        }
-
-
-        $query = $dbh->prepare("CALL sp_GetCharacters(?)");
-        $query->bindParam(1, $playerID, PDO::PARAM_INT);
-        $query->execute();
-        $returnObject = $query->fetchAll(PDO::FETCH_ASSOC);
-
-
-        //done with database connection. Closing connection.
-        $query->closeCursor();
-
-        return $returnObject;
-
-    }
-
-    public function createPlayer($register)
-    {
-        // TODO: Implement createPlayer() method.
-        //create database reference object
-        $dbh='';
-
-        //Try to connect to mysql service
-        try
-        {
-            //created config file to hold user name and password that we will use to obscure and keep off of our repo.
-            $dbh = new PDO("mysql:host=localhost:3306;dbname=dbo", dbuser, dbpass);
-        }
-        catch(PDOException $e)
-        {
-            //this will be ignored in production. do not want to echo back this error.
-            echo $e->getMessage();
-        }
-
-        $query = $dbh->prepare("CALL sp_CreatePlayer(?,?,?,?,?)");
-        $query->bindParam(1,$register->{"username"},PDO::PARAM_STR);
-        $query->bindParam(2,$register->{"password"},PDO::PARAM_STR);
-        $query->bindParam(3,$register->{"firstname"},PDO::PARAM_STR);
-        $query->bindParam(4,$register->{"lastname"},PDO::PARAM_STR);
-        $query->bindParam(5,$register->{"email"},PDO::PARAM_STR);
-
-        $query->execute();
-
-        $return = null;
-
-        if($query->rowCount() == 1)
-        {
-            $return["PlayerInfo"] = $this->authenticateUser($register->{"username"}, $register->{"password"});
-            $return["PlayerDetails"] = $this->getPlayer($return["PlayerInfo"]["playerId"]);
+            //return status code to be used in response message. 200 successful.
+            $returnCode = 200;
         }
 
         $query->closeCursor();
 
-        return $return;
-
-
+        return $returnCode;
     }
-
-
 }
