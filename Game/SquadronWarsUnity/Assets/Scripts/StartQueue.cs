7f3bbe257aad14ca040d11a1868c2c80518ce3ea
Brandon Richardson <Brandon Richardson>
2016-03-24 15:20:31 -0600

    missed files

--- Game/SquadronWarsUnity/Assets/Scripts/StartQueue.cs
+++ Game/SquadronWarsUnity/Assets/Scripts/StartQueue.cs
@@ -1,32 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.Data;
+using Assets.GameClasses;
 using UnityEngine.SceneManagement;
 
 public class StartQueue : MonoBehaviour {
 
-    public GameObject homeScreen;
+    public GameObject gameScreen;
     public GameObject queueScreen;
+    private DbConnection _dbConnection = new DbConnection();
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        	
 	}
 
     public void StartFindingMatch()
     {
         //homeScreen.SetActive(false);
         queueScreen.SetActive(true);
-        StartCoroutine("FindPlayer");
+        WaitForGameInfoReturned();
+        SceneManager.LoadScene("BattleMap2");
+        //StartCoroutine("FindPlayer");
         
     }
 
+    /*
     IEnumerator FindPlayer()
     {
         yield return new WaitForSeconds(.5f);
         SceneManager.LoadScene("BattleMap2");
+    }
+    */
+
+    public void WaitForGameInfoReturned()
+    {
+        //StartCoroutine(GlobalConstants.Utilities.GetGameInfo(GlobalConstants.StartGameUrl));
+        var gameInfo = GlobalConstants.Utilities.GetGameInfo(GlobalConstants.StartGameUrl, _dbConnection);
+
+        //var gameInfo = GlobalConstants.GameInfo;
+        if (gameInfo != null)
+        {
+            GlobalConstants.Utilities.UpdateGame(gameInfo);
+            GlobalConstants.Updated = true;
+        }
     }
 }
